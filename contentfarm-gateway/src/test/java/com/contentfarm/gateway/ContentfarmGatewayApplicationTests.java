88fd72deb9c7fa46a63208fa7eccf42478d1b124
kenwu565657 <kenwu565656@icloud.com>
2025-01-26 22:59:51 +0800

    add spring cloud gateway test

---
+++ contentfarm-gateway/src/test/java/com/contentfarm/gateway/ContentfarmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentfarm.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentfarmGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
