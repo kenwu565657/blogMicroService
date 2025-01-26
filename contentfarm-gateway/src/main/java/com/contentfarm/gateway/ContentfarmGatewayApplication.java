2c038e61ab9935988b3140d8d6430ed1a1f978ae
kenwu565657 <kenwu565656@icloud.com>
2025-01-26 22:58:55 +0800

    add spring cloud gateway

---
+++ contentfarm-gateway/src/main/java/com/contentfarm/gateway/ContentfarmGatewayApplication.java
@@ -0,0 +1,30 @@
+package com.contentfarm.gateway;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
+public class ContentfarmGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentfarmGatewayApplication.class, args);
+    }
+
+    @Bean
+    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+                .route(r -> r.path("/blogpost-service/v3/api-docs").and().method(HttpMethod.GET).uri("lb://blogpost-service"))
+                .build();
+    }
+}
