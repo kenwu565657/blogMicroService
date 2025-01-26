e7cb7661b377cb0378829942a60e064377362808
kenwu565657 <kenwu565656@icloud.com>
2025-01-26 23:02:02 +0800

    add discovery server

---
+++ contentfarm-eureka/src/main/java/com/contentfarm/eureka/ContentfarmEurekaApplication.java
@@ -0,0 +1,15 @@
+package com.contentfarm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ContentfarmEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContentfarmEurekaApplication.class, args);
+    }
+
+}
---
+++ contentfarm-eureka/src/test/java/com/contentfarm/eureka/ContentfarmEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentfarm.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentfarmEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
