0ce50186216d8d6fa6e266f9681bd57fa1a2a73d
kenwu565657 <kenwu565656@icloud.com>
2025-01-31 01:04:17 +0800

    remove unused code

--- contentfarm-persistence/src/main/java/com/contentfarm/persistence/App.java
+++
@@ -1,7 +0,0 @@
-package com.contentfarm.persistence;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class App {
-}
--- contentfarm-persistence/src/main/java/com/contentfarm/persistence/searchcriteria/blogpost/BlogPostSearchCriteria.java
+++
@@ -1,9 +0,0 @@
-package com.contentfarm.persistence.searchcriteria.blogpost;
-
-import com.contentfarm.persistence.searchcriteria.BaseSearchCriteria;
-import lombok.experimental.SuperBuilder;
-
-@SuperBuilder
-public class BlogPostSearchCriteria extends BaseSearchCriteria {
-    private String id;
-}
