3fb9e86b8aa597ff002943946cf98e0c0d2858b4
kenwu565657 <kenwu565656@icloud.com>
2025-02-27 07:23:47 +0800

    remove unused module

--- contentfarm-domain/src/main/java/com/contentfarm/contentfarmdomain/outputport/blogpost/IBlogPostPersistenceService.java
+++
@@ -1,7 +0,0 @@
-package com.contentfarm.contentfarmdomain.outputport.blogpost;
-
-import com.contentfarm.contentfarmdomain.aggregateroot.blogpost.BlogPostDomainModel;
-
-public interface IBlogPostPersistenceService {
-    void save(BlogPostDomainModel blogPostDomainModel);
-}
