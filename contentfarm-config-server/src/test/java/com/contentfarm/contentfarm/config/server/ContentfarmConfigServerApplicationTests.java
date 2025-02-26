ce3fd25578088442c697b66b0b8f27be6b028885
kenwu565657 <kenwu565656@icloud.com>
2025-02-27 07:28:06 +0800

    add config server

---
+++ contentfarm-config-server/src/test/java/com/contentfarm/contentfarm/config/server/ContentfarmConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentfarm.contentfarm.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentfarmConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
