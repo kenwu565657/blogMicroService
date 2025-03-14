87f810a3e53ad9c148bdfe3c83e23b9817664015
kenwu565657 <kenwu565656@icloud.com>
2025-03-14 11:52:48 +0800

    update gateway prod config

--- contentfarm-gateway/src/main/java/com/contentfarm/gateway/ContentfarmGatewayApplication.java
+++ contentfarm-gateway/src/main/java/com/contentfarm/gateway/ContentfarmGatewayApplication.java
@@ -2,6 +2,7 @@ package com.contentfarm.gateway;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,13 +13,17 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
+
+import java.text.MessageFormat;
 
 @EnableWebFluxSecurity
 @EnableDiscoveryClient
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
 public class ContentfarmGatewayApplication {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String authServerUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(ContentfarmGatewayApplication.class, args);
@@ -36,6 +41,8 @@ public class ContentfarmGatewayApplication {
 
     @Bean
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        final String jwkUri = MessageFormat.format("{0}/oauth2/jwks", authServerUrl);
+
         http
                 .authorizeExchange((authorize) -> authorize
                         .pathMatchers(HttpMethod.GET, "/blogpost/**").permitAll()
@@ -44,10 +51,10 @@ public class ContentfarmGatewayApplication {
                         .anyExchange().authenticated()
                 )
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
+                //.securityContextRepository(NoOpServerSecurityContextRepository.getInstance())
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
-                                .jwkSetUri("http://localhost:9000/oauth2/jwks")
+                                .jwkSetUri(jwkUri)
                         )
                 )
                 ;
