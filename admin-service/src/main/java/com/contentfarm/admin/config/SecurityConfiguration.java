1e235448a6d63acca00c1798c9b99ede0b4cd76c
kenwu565657 <kenwu565656@icloud.com>
2025-03-14 11:43:35 +0800

    add production config to admin service

--- admin-service/src/main/java/com/contentfarm/admin/config/SecurityConfiguration.java
+++ admin-service/src/main/java/com/contentfarm/admin/config/SecurityConfiguration.java
@@ -1,19 +1,26 @@
 package com.contentfarm.admin.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.text.MessageFormat;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String authServerUrl;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        final String jwkUri = MessageFormat.format("{0}/oauth2/jwks", authServerUrl);
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
@@ -24,7 +31,7 @@ public class SecurityConfiguration {
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
-                                .jwkSetUri("http://localhost:9000/oauth2/jwks")
+                                .jwkSetUri(jwkUri)
                         )
                 )
                 ;
---
+++ admin-service/src/test/java/com/contentfarm/admin/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contentfarm.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
